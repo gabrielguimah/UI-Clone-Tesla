dc3b7787f12b793241a82fd35ce9fd75758e6153
gabriel <gabrielguimah@gmail.com>
2021-07-13 14:18:42 -0300

    Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { GlobalStyles } from "./styles/GlobalStyles";
+import Page from "./Components/Page";
+
+function App() {
+  return (
+    <>
+      <Page />
+
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Page/styles.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  .colored:nth-child(1) {
+    background: #e3f2fd;
+  }
+  .colored:nth-child(2) {
+    background: #bbdefb;
+  }
+  .colored:nth-child(3) {
+    background: #90caf9;
+  }
+  .colored:nth-child(4) {
+    background: #64b5f6;
+  }
+  .colored:nth-child(5) {
+    background: #42a5f5;
+  }
+  .colored:nth-child(6) {
+    background: #2196f3;
+  }
+  .colored:nth-child(7) {
+    background: #1e88e5;
+  }
+`;
+
+export const Spacer = styled.div`
+  height: 15vh;
+  background: #1e88e5;
+`;
